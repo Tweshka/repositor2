fe31116d48b9ec3e0a5fc70a4c84a383a177b165
Name <email@example.ru>
2024-05-03 14:57:18 +0300

    Добавьте файлы проекта.

---
+++ repositor2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace repositor2
+
+{
+    internal class Program;
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DaysOfWeek MyFavoriteDay;
+
+            MyFavoriteDay = DaysOfWeek.sunday;
+
+            Console.WriteLine(MyFavoriteDay);
+        }
+    }
+
+    enum DaysOfWeek : byte
+    {
+        Tuesday,
+        Monday,
+        Wednesday,
+        Friday,
+        sunday
+    }
+}
+
+
+

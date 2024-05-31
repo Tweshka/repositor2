b60022355f9a01314090023240e087b737ffbc60
Name <email@example.ru>
2024-05-31 15:30:17 +0300

    Итоговый проект 5.6.1 (HW-03)

--- repositor2/Program.cs
+++ repositor2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class HelloWorld
@@ -8,36 +9,100 @@ class HelloWorld
 
     public static void Main(string[] args)
     {
-        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+        Console.WriteLine("Введите имя:");
+        string firstName = Console.ReadLine();
 
-        var color = Console.ReadLine();
+        Console.WriteLine("Введите фамилию:");
+        string lastName = Console.ReadLine();
 
-        if (color == "red")
+        int age = GetValidNumber("Введите возраст:");
+
+        Console.WriteLine("Есть ли у вас питомец? (да/нет):");
+        bool hasPet = Console.ReadLine() == "да";
+
+        string[] petNames = null;
+        if (hasPet)
         {
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-
-            Console.WriteLine("Your color is red!");
+            int pet = GetValidNumber("Введите количество питомцев:");
+            petNames = GetPetNames(pet);
         }
 
-        else if (color == "green")
-        {
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.Black;
+        int favColor = GetValidNumber("Введите количество любимых цветов:");
+        string[] favoriteColors = GetFavoriteColors(favColor);
 
-            Console.WriteLine("Your color is green!");
-        }
-        else
-        {
-            Console.BackgroundColor = ConsoleColor.Cyan;
-            Console.ForegroundColor = ConsoleColor.Black;
-
-            Console.WriteLine("Your color is cyan!");
-
-
-        }
+       
     }
+
+    static int GetValidNumber(string message)
+    {
+        int number;
+        bool isValidInput;
+        do
+        {
+            Console.WriteLine(message);
+            isValidInput = int.TryParse(Console.ReadLine(), out number) && number > 0;
+            if (!isValidInput)
+            {
+                Console.WriteLine("Пожалуйста, введите корректное число (целое число больше 0).");
+            }
+        } while (!isValidInput);
+        return number;
+    }
+
+    static string[] GetPetNames(int count)
+    {
+        string[] petNames = new string[count];
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine($"Введите кличку питомца {i + 1}:");
+            petNames[i] = Console.ReadLine();
+        }
+        return petNames;
+    }
+
+    static string[] GetFavoriteColors(int count)
+    {
+        string[] favoriteColors = new string[count];
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine($"Введите любимый цвет {i + 1}:");
+            favoriteColors[i] = Console.ReadLine();
+        }
+        return favoriteColors;
+    }
+
+   
+
 }
+
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
